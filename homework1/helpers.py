7a561b5f1770faa44fcc7ee1f3f395a17871be33
AdrianDochian <adrian.dochian667@protonmail.com>
2021-11-12 17:38:26 +0200

    deleted not relevant python code. Moved TicTacToe implementation to project root. Created README.md file

--- homework1/helpers.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/pyton3
-
-def isNumber(text):
-	for atom in text:
-		if atom < '0' or atom > '9':
-			return False
-	return True
--- homework1/pro2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/pyton3
-
-number = int(input("Introdu un număr: "))
-
-if number % 2 == 0: 
-	print("Numărul {} este par".format(number))
-else:
-	print("Numărul {} este impar".format(number))
